7369134268aed9c36233041834d2ba5af92a2705
BoiHanny <hannoboy@protonmail.com>
2022-09-28 17:41:39 +0200

    Settings up classes and the viewmodel.

--- vrcosc-magicchatbox/MainWindow.xaml.cs
+++ vrcosc-magicchatbox/MainWindow.xaml.cs
@@ -3,9 +3,6 @@
 
 namespace vrcosc_magicchatbox
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
---
+++ vrcosc-magicchatbox/ViewModels/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+
+namespace vrcosc_magicchatbox.ViewModels
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        #region Properties
+
+        #endregion
+
+        #region PropChangedEvent
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string name)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+    }
+}
