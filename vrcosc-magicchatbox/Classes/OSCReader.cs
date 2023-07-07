31602b62020acf874ccc7099d02f7eadd541f324
BoiHanny <hannoboy@protonmail.com>
2023-07-07 18:31:59 +0200

    Fixed bug in OSCReader

--- vrcosc-magicchatbox/Classes/OSCReader.cs
+++ vrcosc-magicchatbox/Classes/OSCReader.cs
@@ -1,71 +1,97 @@
 ﻿using CoreOSC;
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Threading;
 using vrcosc_magicchatbox.Classes.DataAndSecurity;
 using vrcosc_magicchatbox.ViewModels;
 
-namespace vrcosc_magicchatbox.Classes
+public static class OSCReader
 {
+    private static UDPListener listener;
+    private static CancellationTokenSource cancellation = new CancellationTokenSource();
 
-    public static class OSCReader
+
+    public static void StartListening()
     {
-        private static UDPListener listener;
-        private static CancellationTokenSource cancellation = new CancellationTokenSource();
-
-
-        public static void StartListening()
+        void callback(OscPacket packet)
         {
-            void callback(OscPacket packet)
+
+            if (packet is OscMessage message && message.Arguments.Count > 0)
             {
-                if (packet is OscMessage message && message.Arguments.Count > 0)
+
+                if (message.Address.StartsWith("/avatar/parameters/"))
                 {
-                    if (message.Address.StartsWith("/avatar/parameters/"))
+
+                    var parameterName = message.Address.Substring("/avatar/parameters/".Length);
+
+                    var parameterProperty = typeof(OSCParameters).GetProperty(parameterName);
+
+                    if (parameterProperty != null)
                     {
-                        var parameterName = message.Address.Substring("/avatar/parameters/".Length);
-                        var parameterProperty = typeof(OSCParameters).GetProperty(parameterName);
+                        // This is a built-in parameter
+                        var parameter = (OSCParameter)parameterProperty.GetValue(null);
+                        parameter.SetValue(message.Arguments[0]);
+                        ViewModel.Instance.BuiltInOSCData[parameterName] = parameter;
 
-                        if (parameterProperty != null)
-                        {
-                            // This is a built-in parameter
-                            var parameter = (OSCParameter)parameterProperty.GetValue(null);
-                            parameter.SetValue(message.Arguments[0]);
-                            ViewModel.Instance.BuiltInOSCData[parameterName] = parameter;
-                        }
-                        else
-                        {
-                            // This is a dynamic parameter
-                            if (!(ViewModel.Instance.DynamicOSCData as IDictionary<string, object>).TryGetValue(parameterName, out var parameterObject))
-                            {
-                                // This is a new parameter
-                                var newParameter = new OSCParameter(parameterName, message.Address, message.Arguments[0].GetType().Name);
-                                (ViewModel.Instance.DynamicOSCData as IDictionary<string, object>)[parameterName] = newParameter;
-                            }
+                    }
+                    else
+                    {
+                        // This is a dynamic parameter
 
-                            var dynamicParameter = parameterObject as OSCParameter;
-                            dynamicParameter.SetValue(message.Arguments[0]);
+                        if (!(ViewModel.Instance.DynamicOSCData as IDictionary<string, object>).TryGetValue(parameterName, out var parameterObject))
+                        {
+                            // This is a new parameter
+                            Logging.WriteInfo("This is a new parameter");
+
+                            var newParameter = new OSCParameter(parameterName, message.Address, message.Arguments[0].GetType().Name);
+                            (ViewModel.Instance.DynamicOSCData as IDictionary<string, object>)[parameterName] = newParameter;
+                            parameterObject = newParameter;
+
+                            Logging.WriteInfo($"New dynamic parameter {parameterName} created with value: {message.Arguments[0]}");
                         }
+
+                        var dynamicParameter = parameterObject as OSCParameter;
+                        dynamicParameter.SetValue(message.Arguments[0]);
+
+                        Logging.WriteInfo($"Dynamic parameter {parameterName} set with value: {message.Arguments[0]}");
                     }
                 }
-            }
+                else
+                {
 
-            if (listener == null)
-            {
-                listener = new UDPListener(ViewModel.Instance.OSCPOrtIN, callback);
+                }
             }
         }
 
-        public static void StopListening()
+        try
+        {
+            if (listener == null)
+            {
+                listener = new UDPListener(ViewModel.Instance.OSCPOrtIN, callback);
+                Logging.WriteInfo("Listener started successfully on port: " + ViewModel.Instance.OSCPOrtIN);
+            }
+        }
+        catch (Exception ex)
+        {
+            Logging.WriteException(ex);
+        }
+    }
+
+    public static void StopListening()
+    {
+        try
         {
             if (listener != null)
             {
                 listener.Dispose();
                 listener = null;
+                Logging.WriteInfo("Listener stopped successfully.");
             }
         }
-
+        catch (Exception ex)
+        {
+            Logging.WriteException(ex);
+        }
     }
-
 
 }
