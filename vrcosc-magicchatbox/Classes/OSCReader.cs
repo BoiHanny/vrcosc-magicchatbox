57a1b52b355b6807dc94cddbedcc5eb1ff4ee64a
BoiHanny <hannoboy@protonmail.com>
2023-07-29 16:37:39 +0200

    Change to OSCReader, logging to on new parameter creation.

--- vrcosc-magicchatbox/Classes/OSCReader.cs
+++ vrcosc-magicchatbox/Classes/OSCReader.cs
@@ -1,7 +1,9 @@
 ﻿using CoreOSC;
 using System;
 using System.Collections.Generic;
+using System.Text;
 using System.Threading;
+using System.Xml.Linq;
 using vrcosc_magicchatbox.Classes.DataAndSecurity;
 using vrcosc_magicchatbox.ViewModels;
 
@@ -13,53 +15,77 @@ public static class OSCReader
 
     public static void StartListening()
     {
+
         void callback(OscPacket packet)
         {
             if (packet is OscMessage message && message.Arguments.Count > 0)
             {
-                if (message.Address.StartsWith("/avatar/parameters/"))
+                string parameterName;
+                var parameterPrefix = "/avatar/parameters/";
+
+                if (message.Address.StartsWith(parameterPrefix))
                 {
-                    var parameterName = message.Address.Substring("/avatar/parameters/".Length);
-
-                    var parameterProperty = typeof(OSCParameters).GetProperty(parameterName);
-
-                    if (parameterProperty != null)
-                    {
-                        // This is a built-in parameter
-                        var parameter = (OSCParameter)parameterProperty.GetValue(null);
-                        parameter.SetValue(message.Arguments[0]);
-                        ViewModel.Instance.BuiltInOSCData[parameterName] = parameter;
-                    }
-                    else
-                    {
-                        // This is a dynamic parameter
-
-                        if (!(ViewModel.Instance.DynamicOSCData as IDictionary<string, object>).TryGetValue(
-                            parameterName,
-                            out var parameterObject))
-                        {
-                            // This is a new parameter
-                            Logging.WriteInfo("This is a new parameter");
-
-                            var newParameter = new OSCParameter(
-                                parameterName,
-                                message.Address,
-                                message.Arguments[0].GetType().Name);
-                            (ViewModel.Instance.DynamicOSCData as IDictionary<string, object>)[parameterName] = newParameter;
-                            parameterObject = newParameter;
-
-                            Logging.WriteInfo(
-                                $"New dynamic parameter {parameterName} created with value: {message.Arguments[0]}");
-                        }
-
-                        var dynamicParameter = parameterObject as OSCParameter;
-                        dynamicParameter.SetValue(message.Arguments[0]);
-
-                        Logging.WriteInfo($"Dynamic parameter {parameterName} set with value: {message.Arguments[0]}");
-                    }
+                    // This is a parameter
+                    parameterName = message.Address.Substring(parameterPrefix.Length);
+                }
+                else if (message.Address == "/avatar/change")
+                {
+                    // This is the avatar change built-in parameter
+                    parameterName = "AvatarChange";
                 }
                 else
                 {
+                    // This is a different built-in parameter
+                    parameterName = message.Address;
+                }
+
+                var parameterProperty = typeof(OSCParameters).GetProperty(parameterName);
+
+                if (parameterProperty != null)
+                {
+                    // This is a built-in parameter
+                    var parameter = (OSCParameter)parameterProperty.GetValue(null);
+                    parameter.SetValue(message.Arguments[0]);
+                    ViewModel.Instance.BuiltInOSCData[parameterName] = parameter;
+                }
+                else
+                {
+                    // This is a dynamic parameter
+                    bool isNewParameter = false;
+                    if (!(ViewModel.Instance.DynamicOSCData as IDictionary<string, object>).TryGetValue(
+                        parameterName,
+                        out var parameterObject))
+                    {
+                        // This is a new parameter
+                        var newParameter = new OSCParameter(
+                            parameterName,
+                            message.Address,
+                            message.Arguments[0].GetType().Name);
+                        (ViewModel.Instance.DynamicOSCData as IDictionary<string, object>)[parameterName] = newParameter;
+                        parameterObject = newParameter;
+                        isNewParameter = true;
+                    }
+
+                    var dynamicParameter = parameterObject as OSCParameter;
+                    dynamicParameter.SetValue(message.Arguments[0]);
+
+                    var sb = new StringBuilder();
+
+                    if(isNewParameter)
+                    {
+                    sb.Append("Dynamic OSCParameter ++ [")
+                      .Append(dynamicParameter.Name)
+                      .Append("] Type: (")
+                      .Append(dynamicParameter.Type.Name)
+                      .Append(") ")
+                      .Append("Added and has been set to: ")
+                      .Append(dynamicParameter.GetValue())
+                      .Append(" | History allowed: ")
+                      .Append(dynamicParameter.MaxHistory);
+                       Logging.WriteInfo(sb.ToString());
+                    }
+                    
+
                 }
             }
         }
@@ -69,7 +95,7 @@ public static class OSCReader
             if (listener == null)
             {
                 listener = new UDPListener(ViewModel.Instance.OSCPOrtIN, callback);
-                Logging.WriteInfo("Listener started successfully on port: " + ViewModel.Instance.OSCPOrtIN);
+                Logging.WriteInfo("OSCParameter listener started successfully on port: " + ViewModel.Instance.OSCPOrtIN);
             }
         }
         catch (Exception ex)
--- vrcosc-magicchatbox/ViewModels/OSCParameter.cs
+++ vrcosc-magicchatbox/ViewModels/OSCParameter.cs
@@ -1,74 +1,107 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using vrcosc_magicchatbox.Classes.DataAndSecurity;
 
 
 
-    public class OSCParameter
+public class OSCParameter
+{
+
+    public OSCParameter(string name, string address, string type, int maxHistory = 5, bool isBuiltIn = false, bool logChange = false)
     {
-        public string Name { get; set; }
-        public string Address { get; set; }
-        public Type Type { get; set; }
-        public LinkedList<object> Values { get; set; }
-        public int MaxHistory { get; set; }
+        Name = name;
+        Address = address;
+        Values = new LinkedList<object>();
+        MaxHistory = maxHistory;
+        IsBuiltIn = isBuiltIn;
+        LogChanges = logChange;
 
-        public OSCParameter(string name, string address, string type, int maxHistory = 5)
+
+        switch (type)
         {
-            Name = name;
-            Address = address;
-            Values = new LinkedList<object>();
-            MaxHistory = maxHistory;
-
-            switch (type)
-            {
-                case "Int32":
-                    Type = typeof(int);
-                    break;
-                case "Single":
-                    Type = typeof(float);
-                    break;
-                case "Boolean":
-                    Type = typeof(bool);
-                    break;
-                default:
-                    throw new ArgumentException($"Invalid parameter type: {type}");
-            }
-        }
-
-        public void SetValue(object value)
-        {
-            if (Values.Count >= MaxHistory)
-            {
-                Values.RemoveFirst();
-            }
-            Values.AddLast(value);
-        }
-
-        public object GetValue()
-        {
-            return Values.Last.Value;
+            case "Int32":
+                Type = typeof(int);
+                break;
+            case "Single":
+                Type = typeof(float);
+                break;
+            case "Boolean":
+                Type = typeof(bool);
+                break;
+            default:
+                throw new ArgumentException($"Invalid parameter type: {type}");
         }
     }
 
+    public object GetValue()
+    {
+        return Values.Last.Value;
+    }
+
+    public void LogBuilder()
+    {
+        var sb = new StringBuilder();
+        if (LogChanges)
+        {
+            sb.Append(IsBuiltIn ? "BuiltIn OSCParameter -> [" : "Dynamic OSCParameter -> [")
+                .Append(Name)
+                .Append("] Type: (")
+                .Append(Type.Name)
+                .Append(") ")
+                .Append("Has been set to: ")
+                .Append(GetValue())
+                .Append(" | History Count: ")
+                .Append(Values.Count)
+                .Append("/")
+                .Append(MaxHistory);
+            Logging.WriteInfo(sb.ToString());
+        }
+        
+    }
+
+
+    public void SetValue(object value)
+    {
+        if (Values.Count >= MaxHistory)
+        {
+            Values.RemoveFirst();
+        }
+        Values.AddLast(value);
+        LogBuilder();
+    }
+
+    public string Address { get; set; }
+    public bool IsBuiltIn { get; set; }
+    public bool LogChanges { get; set; }
+    public int MaxHistory { get; set; }
+    public string Name { get; set; }
+    public Type Type { get; set; }
+    public LinkedList<object> Values { get; set; }
+}
+
 public static class OSCParameters
 {
-    public static OSCParameter Face { get; private set; } = new OSCParameter("Face", "/avatar/parameters/Face", "Int32", 5);
-    public static OSCParameter VelocityZ { get; private set; } = new OSCParameter("VelocityZ", "/avatar/parameters/VelocityZ", "Single", 5);
-    public static OSCParameter VelocityY { get; private set; } = new OSCParameter("VelocityY", "/avatar/parameters/VelocityY", "Single", 5);
-    public static OSCParameter VelocityX { get; private set; } = new OSCParameter("VelocityX", "/avatar/parameters/VelocityX", "Single", 5);
-    public static OSCParameter InStation { get; private set; } = new OSCParameter("InStation", "/avatar/parameters/InStation", "Boolean", 5);
-    public static OSCParameter Seated { get; private set; } = new OSCParameter("Seated", "/avatar/parameters/Seated", "Boolean", 5);
-    public static OSCParameter AFK { get; private set; } = new OSCParameter("AFK", "/avatar/parameters/AFK", "Boolean", 5);
-    public static OSCParameter Upright { get; private set; } = new OSCParameter("Upright", "/avatar/parameters/Upright", "Single", 5);
-    public static OSCParameter VelocityMagnitude { get; private set; } = new OSCParameter("VelocityMagnitude", "/avatar/parameters/VelocityMagnitude", "Single", 5);
-    public static OSCParameter AngularY { get; private set; } = new OSCParameter("AngularY", "/avatar/parameters/AngularY", "Single", 5);
-    public static OSCParameter Grounded { get; private set; } = new OSCParameter("Grounded", "/avatar/parameters/Grounded", "Boolean", 5);
-    public static OSCParameter MuteSelf { get; private set; } = new OSCParameter("MuteSelf", "/avatar/parameters/MuteSelf", "Boolean", 5);
-    public static OSCParameter VRMode { get; private set; } = new OSCParameter("VRMode", "/avatar/parameters/VRMode", "Int32", 5);
-    public static OSCParameter TrackingType { get; private set; } = new OSCParameter("TrackingType", "/avatar/parameters/TrackingType", "Int32", 5);
-    public static OSCParameter GestureRightWeight { get; private set; } = new OSCParameter("GestureRightWeight", "/avatar/parameters/GestureRightWeight", "Single", 5);
-    public static OSCParameter GestureRight { get; private set; } = new OSCParameter("GestureRight", "/avatar/parameters/GestureRight", "Int32", 5);
-    public static OSCParameter GestureLeftWeight { get; private set; } = new OSCParameter("GestureLeftWeight", "/avatar/parameters/GestureLeftWeight", "Single", 5);
-    public static OSCParameter GestureLeft { get; private set; } = new OSCParameter("GestureLeft", "/avatar/parameters/GestureLeft", "Int32", 5);
-    public static OSCParameter Viseme { get; private set; } = new OSCParameter("Viseme", "/avatar/parameters/Viseme", "Int32", 5);
+    public static OSCParameter AFK { get; private set; } = new OSCParameter("AFK", "/avatar/parameters/AFK", "Boolean", 5, true, true);
+    public static OSCParameter AngularY { get; private set; } = new OSCParameter("AngularY", "/avatar/parameters/AngularY", "Single", 5, true);
+    public static OSCParameter AvatarChange { get; } = new OSCParameter("AvatarChange", "/avatar/AvatarChange", "Int32", 5, true, true);
+    public static OSCParameter Face { get; private set; } = new OSCParameter("Face", "/avatar/parameters/Face", "Int32", 5, true, false);
+    public static OSCParameter GestureLeft { get; private set; } = new OSCParameter("GestureLeft", "/avatar/parameters/GestureLeft", "Int32", 5, true, true);
+    public static OSCParameter GestureLeftWeight { get; private set; } = new OSCParameter("GestureLeftWeight", "/avatar/parameters/GestureLeftWeight", "Single", 5, true, true);
+    public static OSCParameter GestureRight { get; private set; } = new OSCParameter("GestureRight", "/avatar/parameters/GestureRight", "Int32", 5, true);
+    public static OSCParameter GestureRightWeight { get; private set; } = new OSCParameter("GestureRightWeight", "/avatar/parameters/GestureRightWeight", "Single", 5, true, true);
+    public static OSCParameter Grounded { get; private set; } = new OSCParameter("Grounded", "/avatar/parameters/Grounded", "Boolean", 5, true, true);
+    public static OSCParameter InStation { get; private set; } = new OSCParameter("InStation", "/avatar/parameters/InStation", "Boolean", 5, true, false);
+    public static OSCParameter MuteSelf { get; private set; } = new OSCParameter("MuteSelf", "/avatar/parameters/MuteSelf", "Boolean", 5, true, true);
+    public static OSCParameter Seated { get; private set; } = new OSCParameter("Seated", "/avatar/parameters/Seated", "Boolean", 5, true, false);
+    public static OSCParameter TrackingType { get; private set; } = new OSCParameter("TrackingType", "/avatar/parameters/TrackingType", "Int32", 5, true, true);
+    public static OSCParameter Upright { get; private set; } = new OSCParameter("Upright", "/avatar/parameters/Upright", "Single", 5, true, true);
+    public static OSCParameter VelocityMagnitude { get; private set; } = new OSCParameter("VelocityMagnitude", "/avatar/parameters/VelocityMagnitude", "Single", 5, true);
+    public static OSCParameter VelocityX { get; private set; } = new OSCParameter("VelocityX", "/avatar/parameters/VelocityX", "Single", 5, true, false);
+    public static OSCParameter VelocityY { get; private set; } = new OSCParameter("VelocityY", "/avatar/parameters/VelocityY", "Single", 5, true, false);
+    public static OSCParameter VelocityZ { get; private set; } = new OSCParameter("VelocityZ", "/avatar/parameters/VelocityZ", "Single", 5, true, false);
+    public static OSCParameter Viseme { get; private set; } = new OSCParameter("Viseme", "/avatar/parameters/Viseme", "Int32", 5, true);
+    public static OSCParameter VRMode { get; private set; } = new OSCParameter("VRMode", "/avatar/parameters/VRMode", "Int32", 5, true, true);
+    public static OSCParameter Earmuffs { get; private set; } = new OSCParameter("Earmuffs", "/avatar/parameters/Earmuffs", "Boolean", 5, true, true);
 
 }
