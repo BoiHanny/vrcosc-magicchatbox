d0d41d2c2f0c4f4d6117fe5b9ee751cd514ec055
BoiHanny <hannoboy@protonmail.com>
2023-09-15 11:17:30 +0200

    Deleted PulsoidOAuthLib

--- vrcosc-magicchatbox/Classes/StatsManager.cs
+++ vrcosc-magicchatbox/Classes/StatsManager.cs
@@ -66,7 +66,7 @@ namespace vrcosc_magicchatbox.Classes
             {
                 Logging.WriteException(ex);
             }
-            
+
         }
 
         private void InitializeDefaultStats()
@@ -120,7 +120,7 @@ namespace vrcosc_magicchatbox.Classes
             {
                 Logging.WriteException(ex);
             }
-           
+
         }
 
 
