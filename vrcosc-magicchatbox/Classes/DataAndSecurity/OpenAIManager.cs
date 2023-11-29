d5aff3bb889ef52b1448a0a15d9949e10ce40a9f
BoiHanny <hannoboy@protonmail.com>
2023-11-29 19:44:05 +0100

    The most significant changes include the addition of a new class `OpenAIManager` in the `OpenAIManager.cs` file and the inclusion of a new package reference to `OpenAI-DotNet` in the `MagicChatbox.csproj` file.
    1. A new class `OpenAIManager` has been added to the `OpenAIManager.cs` file. This class is part of the `vrcosc_magicchatbox.Classes.DataAndSecurity` namespace and uses the `OpenAI` and `System` namespaces. It has a private constructor and a public static method `Create` that takes an API key and an organization ID as parameters. This method creates an instance of `OpenAIManager`, sets the `Client` property using the `SetClient` method, and returns the instance if the client was set successfully. If the client was not set successfully, it returns null. The `SetClient` method tries to set the `Client` property and returns a boolean indicating whether it was successful. If an exception occurs, it logs the exception and returns false. (Refer to `OpenAIManager.cs`)
    
    2. The `MagicChatbox.csproj` file has been updated to include a new package reference to `OpenAI-DotNet` version `7.3.8`. This update allows the project to use the functionalities provided by the `OpenAI-DotNet` package. (Refer to `MagicChatbox.csproj`)

---
+++ vrcosc-magicchatbox/Classes/DataAndSecurity/OpenAIManager.cs
@@ -0,0 +1,48 @@
+﻿using OpenAI;
+using System;
+
+namespace vrcosc_magicchatbox.Classes.DataAndSecurity
+{
+    public class OpenAIManager
+    {
+        public OpenAIClient Client;
+
+        private OpenAIManager()
+        {
+        }
+
+        public static OpenAIManager Create(string apiKey, string organizationID)
+        {
+            OpenAIManager StartedAIManager = new OpenAIManager();
+            if (string.IsNullOrEmpty(apiKey))
+            {
+                throw new ArgumentNullException(nameof(apiKey));
+            }
+            bool HasBeenSet = StartedAIManager.SetClient(new OpenAIClient(new OpenAIAuthentication(apiKey, organizationID)));
+            if (HasBeenSet)
+            {
+                return StartedAIManager;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public bool SetClient(OpenAIClient client)
+        {
+            try
+            {
+                Client = client;
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                Logging.WriteException(ex);
+                return false;
+            }
+
+        }
+    }
+}
