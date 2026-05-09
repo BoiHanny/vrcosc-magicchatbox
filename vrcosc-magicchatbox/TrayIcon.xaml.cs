398463d29873b544c55a75fb3542439ec52c76b4
chaixshot <chai5142xshot@gmail.com>
2026-05-09 14:26:03 +0700

    Added TrayIcon

---
+++ vrcosc-magicchatbox/TrayIcon.xaml.cs
@@ -0,0 +1,32 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using NullSoftware.ToolKit;
+using System.Windows;
+using System.Windows.Input;
+
+namespace vrcosc_magicchatbox
+{
+    /// <summary>
+    /// Interaction logic for TrayIcon.xaml
+    /// </summary>
+    public partial class TrayIcon : Window
+    {
+        private MainWindow mainWindow = App.mainWindow;
+        public INotificationService NotificationService { get; set; }
+
+        public TrayIcon()
+        {
+            InitializeComponent();
+
+            Loaded += (s, e) =>
+            {
+                DataContext = this;
+                this.Hide();
+            };
+
+        }
+        public void Notify(string text)
+        {
+            NotificationService.Notify("MagicChatbox", text);
+        }
+    }
+}
